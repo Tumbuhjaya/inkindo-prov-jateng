c26d13e99685ed80d313689a9b86f5914377afd3
unknown <reikirayuki@gmail.com>
2026-07-07 15:13:03 +0700

    anggota

--- isine/anggota.js
+++ isine/anggota.js
@@ -81,7 +81,6 @@ router.get('/edit/:id', cek_login, function(req, res) {
 
     let sql2 = `SELECT  *  FROM anggota p where no_anggota = ? `
     let anggota_cek =await sql_enak.raw(sql2,[post["no_anggota"]])
-    console.log(anggota_cek);
 
     if (anggota_cek[0].length==0) {
 
@@ -95,8 +94,6 @@ router.get('/edit/:id', cek_login, function(req, res) {
       res.status(200).json({ status: 200, message: "sukses", data: data})
    })
    .catch(err=>{
-            console.log('error err');
-
     console.log(err,'err');
       res.status(500).json({ status: 500, message: "gagal", data: err})
    })
@@ -136,10 +133,7 @@ router.get('/edit/:id', cek_login, function(req, res) {
       }
     }
     try {
-      console.log('data');
-
       await sql_enak('anggota').where('id','=',post.id).update(post).then(data=>{
-        console.log('data2');
 
         console.log(data);
 
@@ -191,202 +185,231 @@ router.get('/edit/:id', cek_login, function(req, res) {
         res.status(500).json({ status: 500, message: "gagal", data: err})
      })
   })
-  router.post('/import', upload.fields([{ name: 'file_excel', maxCount: 1 }]), async function(req, res){
-    console.log(req.files, req.body);
+router.post('/import', upload.fields([{ name: 'file_excel', maxCount: 1 }]), async function (req, res) {
 
     try {
-      if (req.files) {
-        if (req.files['file_excel']) {
-          let file = req.files.file_excel[0];
-          let hasil = await importExcel({
+
+        if (!req.files || !req.files.file_excel) {
+            return res.json({
+                status: 400,
+                message: 'File tidak ditemukan'
+            });
+        }
+
+        let file = req.files.file_excel[0];
+
+        let hasil = await importExcel({
             sourceFile: file.path,
-            header: { rows: 1 },
-            columnToKey: {
-              C: "no_anggota",
-              D: "nama_perusahaan",
-              E: "penanggung_jawab",
-              F: "tanggal_lahir",
-              G: "gelar_pjbu",
-              H: "alamat",
-              I: "kota",
-              J: "kode_kota",
-              K: "korwil",
-              L: "telepon",
-              M: "fax",
-              N: "status_keanggotaan",
-              O: "disyahkan",
-              R: "yang_hadir",
-              S: "di_undang",
-              T: "pelantikan",
-              Z: "nomor_hp_pjbu",
-              AA: "email_kta",
-              AC: "no_sertifikat",
-              AD: "waktu_penataran",
-              AE: "memiliki_sertifikat_kode_etik",
-              AF: "kta_2020",
-              AG: "kualifikasi",
-              AO: "jumlah_tunggakan",
-              AQ: "ket_tunggakan",
-              U: "iuran2021",
-              V: "iuran2022",
-              W: "iuran2023",
-              X: "iuran2024",
-              Y: "iuran2025"
+            header: {
+                rows: 1
             },
-            sheets: ["JUNI"],
-          });
+            sheets: ["Sheet1"],
+            columnToKey: {
 
-          let result = hasil["JUNI"];
-          let arr = [];
-          let pesan = 'Sukses';
-          let successCount = 0;
-          let errorCount = 0;
+    C: 'no_anggota',
+    D: 'nama_perusahaan',
+    E: 'penanggung_jawab',
+    F: 'tanggal_lahir',
+    G: 'gelar_pjbu',
+    H: 'alamat',
+    I: 'kota',
+    J: 'kode_kota',
+    K: 'korwil',
+    L: 'telepon',
+    M: 'fax',
+    N: 'status_keanggotaan',
+    O: 'disyahkan',
+    P: 'kode_etik',
 
-          for (let i = 0; i < result.length; i++) {
+    R: 'yang_hadir',
+    S: 'di_undang',
+    T: 'pelantikan',
+
+    U: 'baru2021',
+    V: 'baru2022',
+    W: 'baru2023',
+    X: 'baru2024',
+    Y: 'baru2025',
+    Z: 'baru2026',
+
+    AA: 'nomor_hp_pjbu',
+    AB: 'email_kta',
+    AC: 'no_sertifikat',
+    AD: 'waktu_penataran',
+    AE: 'memiliki_sertifikat_kode_etik',
+    AF: 'kta_2020',
+    AG: 'kualifikasi',
+
+    AH: 'bayar2020',
+    AI: 'bayar2021',
+    AJ: 'bayar2022',
+    AK: 'bayar2023',
+    AL: 'bayar2024',
+    AM: 'bayar2025',
+    AN: 'bayar2026'
+
+}
+        });
+
+        let result = hasil["Sheet1"];
+
+        let arr = [];
+        let successCount = 0;
+        let errorCount = 0;
+
+        for (let i = 0; i < result.length; i++) {
+
             let row = result[i];
 
-            // Skip jika no_anggota kosong
-            if (!row.no_anggota) {
-              console.log('Skip row ' + (i + 1) + ' - no_anggota kosong');
-              errorCount++;
-              continue;
+            if (!row.no_anggota || !row.nama_perusahaan) {
+                errorCount++;
+                continue;
             }
 
-            // Skip jika nama_perusahaan kosong
-            if (!row.nama_perusahaan) {
-              console.log('Skip row ' + (i + 1) + ' - nama_perusahaan kosong');
-              errorCount++;
-              continue;
-            }
+            //--------------------------------------
+            // cari tahun masuk
+            //--------------------------------------
 
-            let post = {};
-            post.no_anggota = row.no_anggota;
-            post.nama_perusahaan = row.nama_perusahaan;
-            post.penanggung_jawab = row.penanggung_jawab;
-            post.gelar_pjbu = row.gelar_pjbu;
-            post.tanggal_lahir = row.tanggal_lahir;
-            post.alamat = row.alamat;
-            post.kota = row.kota;
-            post.kode_kota = row.kode_kota;
-            post.korwil = row.korwil;
-            post.telepon = row.telepon;
-            post.fax = row.fax;
-            post.status_keanggotaan = row.status_keanggotaan;
-            post.disyahkan = row.disyahkan;
-            post.yang_hadir = row.yang_hadir;
-            post.di_undang = row.di_undang;
-            post.pelantikan = row.pelantikan;
-            post.nomor_hp_pjbu = row.nomor_hp_pjbu;
-            post.email_kta = row.email_kta;
-            post.no_sertifikat = row.no_sertifikat;
-            post.waktu_penataran = row.waktu_penataran;
-            post.memiliki_sertifikat_kode_etik = row.memiliki_sertifikat_kode_etik;
-            post.kta_2020 = row.kta_2020;
-            post.kualifikasi = row.kualifikasi;
-            post.jumlah_tunggakan = row.jumlah_tunggakan || 0;
-            post.ket_tunggakan = row.ket_tunggakan;
+            let tahun_masuk = null;
+
+            if (row.baru2021 && String(row.baru2021).toUpperCase() == "BARU") tahun_masuk = 2021;
+            else if (row.baru2022 && String(row.baru2022).toUpperCase() == "BARU") tahun_masuk = 2022;
+            else if (row.baru2023 && String(row.baru2023).toUpperCase() == "BARU") tahun_masuk = 2023;
+            else if (row.baru2024 && String(row.baru2024).toUpperCase() == "BARU") tahun_masuk = 2024;
+            else if (row.baru2025 && String(row.baru2025).toUpperCase() == "BARU") tahun_masuk = 2025;
+            else if (row.baru2026 && String(row.baru2026).toUpperCase() == "BARU") tahun_masuk = 2026;
+
+            //--------------------------------------
+            // data anggota
+            //--------------------------------------
+
+            let post = {
+
+                no_anggota: row.no_anggota,
+                nama_perusahaan: row.nama_perusahaan,
+                penanggung_jawab: row.penanggung_jawab,
+                gelar_pjbu: row.gelar_pjbu,
+                tanggal_lahir: row.tanggal_lahir,
+                alamat: row.alamat,
+                kota: row.kota,
+                kode_kota: row.kode_kota,
+                korwil: row.korwil,
+                telepon: row.telepon,
+                fax: row.fax,
+                nomor_hp_pjbu: row.nomor_hp_pjbu,
+                email_kta: row.email_kta,
+                status_keanggotaan: row.status_keanggotaan,
+                disyahkan: row.disyahkan,
+                kode_etik: row.kode_etik,
+                yang_hadir: row.yang_hadir,
+                di_undang: row.di_undang,
+                pelantikan: row.pelantikan,
+                no_sertifikat: row.no_sertifikat,
+                waktu_penataran: row.waktu_penataran,
+                memiliki_sertifikat_kode_etik: row.memiliki_sertifikat_kode_etik,
+                kta_2020: row.kta_2020,
+                kualifikasi: row.kualifikasi,
+                tahun_masuk: tahun_masuk
+
+            };
+
+            //--------------------------------------
+            // cek anggota
+            //--------------------------------------
+
+            let anggota = await sql_enak("anggota")
+                .where("no_anggota", row.no_anggota)
+                .first();
 
             let anggotaId;
 
-            // Cek apakah anggota sudah ada
-            let existing = await sql_enak("anggota")
-              .where("no_anggota", row.no_anggota)
-              .first();
+            if (anggota) {
 
-            if (existing) {
-              // Update anggota yang sudah ada
-              await sql_enak("anggota")
-                .where("id", existing.id)
-                .update(post).then(function() {
-                  successCount++;
-                  anggotaId = existing.id;
+                anggotaId = anggota.id;
 
-                  // Update iuran tahunan
-                  for (let tahun = 2021; tahun <= 2025; tahun++) {
-                    let iuranKey = 'iuran' + tahun;
-                    let statusIuran = row[iuranKey];
+                await sql_enak("anggota")
+                    .where("id", anggotaId)
+                    .update(post);
 
-                    if (statusIuran) {
-                      sql_enak("iuran_tahunan")
-                        .where({
-                          anggota_id: anggotaId,
-                          tahun: tahun
-                        })
-                        .first()
-                        .then(function(cek) {
-                          if (cek) {
-                            return sql_enak("iuran_tahunan")
-                              .where("id", cek.id)
-                              .update({status_pembayaran: statusIuran});
-                          } else {
-                            return sql_enak.insert({
-                              anggota_id: anggotaId,
-                              tahun: tahun,
-                              status_pembayaran: statusIuran
-                            }).into("iuran_tahunan");
-                          }
-                        })
-                        .catch(function(err) {
-                          console.log('Error update iuran:', err);
-                        });
-                    }
-                  }
-                })
-                .catch(function(err) {
-                  console.log('Error update anggota:', err);
-                  errorCount++;
-                });
             } else {
-              // Insert anggota baru
-              await sql_enak.insert(post).into("anggota").then(async function(hsl) {
+
+                let hsl = await sql_enak("anggota")
+                    .insert(post);
+
                 anggotaId = hsl[0];
+
                 arr.push(anggotaId);
-                successCount++;
 
-                // Insert iuran tahunan
-                for (let tahun = 2021; tahun <= 2025; tahun++) {
-                  let iuranKey = 'iuran' + tahun;
-                  let statusIuran = row[iuranKey];
+            }
 
-                  if (statusIuran) {
-                    await sql_enak.insert({
-                      anggota_id: anggotaId,
-                      tahun: tahun,
-                      status_pembayaran: statusIuran
-                    }).into("iuran_tahunan").catch(function(err) {
-                      console.log('Error insert iuran:', err);
-                    });
-                  }
+            //--------------------------------------
+            // pembayaran
+            //--------------------------------------
+
+            const pembayaran = [
+                 row.bayar2020,
+                 row.bayar2021,
+                 row.bayar2022,
+                 row.bayar2023,
+                 row.bayar2024,
+                 row.bayar2025,
+                 row.bayar2026
+            ]
+
+            for (let i = 0; i <pembayaran.length; i++) {
+let tahun = i+2020
+
+                let status = pembayaran[i];
+
+                if (!status) continue;
+
+                let cek = await sql_enak("pembayaran")
+                    .where({
+                        anggota_id: anggotaId,
+                        tahun: tahun
+                    })
+                    .first();
+
+                if (cek) {
+
+                    await sql_enak("pembayaran")
+                        .where("id", cek.id)
+                        .update({
+                            status: status
+                        });
+
+                } else {
+
+                    await sql_enak("pembayaran")
+                        .insert({
+                            anggota_id: anggotaId,
+                            tahun: tahun,
+                            status: status
+                        });
+
                 }
-              }).catch(function(err) {
-                console.log('Error insert anggota:', err);
-                pesan = 'Error, terjadi kesalahan upload pada baris ' + (i + 1);
-                errorCount++;
-              });
-            }
-          }
 
-          console.log('Import selesai: ' + successCount + ' berhasil, ' + errorCount + ' error');
-
-          if (pesan == 'Sukses') {
-            res.json({status: 200, message: 'Import berhasil. ' + successCount + ' data diproses, ' + errorCount + ' error'});
-          } else {
-            // Rollback data yang sudah diinsert
-            for (let k = 0; k < arr.length; k++) {
-              await sql_enak('anggota').where('id', arr[k]).del();
-              await sql_enak('iuran_tahunan').where('anggota_id', arr[k]).del();
             }
-            res.json({status: 500, message: pesan});
-          }
-        } else {
-          res.json({status: 400, message: 'File tidak ditemukan'});
+
+            successCount++;
+
         }
-      } else {
-        res.json({status: 400, message: 'Error upload'});
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).send('Internal Server Error: ' + error.message);
+
+        res.json({
+            status: 200,
+            message: `Import selesai. ${successCount} berhasil, ${errorCount} gagal`
+        });
+
+    } catch (err) {
+
+        console.log(err);
+
+        res.json({
+            status: 500,
+            message: err.message
+        });
+
     }
-  });
+
+});
 module.exports = router;
